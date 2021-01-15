1f93cff6eee4dad066faadcea3a7b7b456bf5a94
FFCBorges <33816@ufp.edu.pt>
2021-01-15 12:27:08 +0000

    small changes across the code to improve imports

--- src/test/java/pt/ufp/edu/projecttracker/repositories/ProjectRepositoryTest.java
+++ src/test/java/pt/ufp/edu/projecttracker/repositories/ProjectRepositoryTest.java
@@ -1,12 +1,11 @@
 package pt.ufp.edu.projecttracker.repositories;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import pt.ufp.edu.projecttracker.model.Project;
 import pt.ufp.edu.projecttracker.model.ProjectManager;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ProjectRepositoryTest {
@@ -31,17 +30,17 @@ class ProjectRepositoryTest {
 //        project.setProjectManager(manager);
 
         ProjectManager manager = new ProjectManager("Manel Manager", "Manel@oBoss.com","passwordDoBoss");
-        assertNotNull(manager.getEmail());
+        Assertions.assertNotNull(manager.getEmail());
         projectManagerRepository.save(manager);
 
 
         Project project = new Project("Projecto Maravilha",manager);
 
-        assertEquals(0,projectRepository.count());
+        Assertions.assertEquals(0, projectRepository.count());
 
         projectRepository.save(project);
 
-        assertEquals(1,projectRepository.count());
+        Assertions.assertEquals(1, projectRepository.count());
 
 
     }
